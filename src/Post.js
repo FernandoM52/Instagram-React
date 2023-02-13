ba8b3a008d57dff07d5ba913a83ab942c873ace3
FernandoM52 <fernandomartins.contato@outlook.com>
2023-02-12 21:35:59 -0300

    Versao dos Posts com map

--- src/Post.js
+++ src/Post.js
@@ -1,45 +1,42 @@
 export default function Post(props) {
     console.log(props);
     return (
-        <ul>
-            <li>
-                <div className="post">
-                    <div className="topo">
-                        <div className="usuario">
-                            <img src={props.fotoUsuario} alt={props.usuarioPost} />
-                            {props.usuarioPost}
+        <li>
+            <div className="post">
+                <div className="topo">
+                    <div className="usuario">
+                        <img src={props.ftUsuario} alt={props.usrPost} />
+                        {props.usrPost}
+                    </div>
+                    <div className="acoes">
+                        <ion-icon name="ellipsis-horizontal"></ion-icon>
+                    </div>
+                </div>
+
+                <div className="conteudo">
+                    <img src={props.conteudo} />
+                </div>
+
+                <div className="fundo">
+                    <div className="acoes">
+                        <div>
+                            <ion-icon name="heart-outline"></ion-icon>
+                            <ion-icon name="chatbubble-outline"></ion-icon>
+                            <ion-icon name="paper-plane-outline"></ion-icon>
                         </div>
-                        <div className="acoes">
-                            <ion-icon name="ellipsis-horizontal"></ion-icon>
+                        <div>
+                            <ion-icon name="bookmark-outline"></ion-icon>
                         </div>
                     </div>
 
-                    <div className="conteudo">
-                        <img src={props.conteudo} />
-                    </div>
-
-                    <div className="fundo">
-                        <div className="acoes">
-                            <div>
-                                <ion-icon name="heart-outline"></ion-icon>
-                                <ion-icon name="chatbubble-outline"></ion-icon>
-                                <ion-icon name="paper-plane-outline"></ion-icon>
-                            </div>
-                            <div>
-                                <ion-icon name="bookmark-outline"></ion-icon>
-                            </div>
-                        </div>
-
-                        <div className="curtidas">
-                            <img src={props.imagemCurtidoPor} />
-                            <div className="texto">
-                                Curtido por <strong>{props.curtidoPor}</strong> e <strong>outras 101.523 pessoas</strong>
-                            </div>
+                    <div className="curtidas">
+                        <img src={props.imgCurtidoPor} />
+                        <div className="texto">
+                            Curtido por <strong>{props.curtidoPor}</strong> e <strong>outras 101.523 pessoas</strong>
                         </div>
                     </div>
                 </div>
-            </li>
-        </ul>
-
+            </div>
+        </li>
     )
 }
--- src/Posts.js
+++ src/Posts.js
@@ -1,12 +1,17 @@
 import Post from "./Post";
 
 export default function Posts() {
+
+    const feed = [
+        { ftUsuario: "./assets/img/meowed.svg", usrPost: "meowed", conteudo: "./assets/img/gato-telefone.svg", imgCurtidoPor: "./assets/img/respondeai.svg", curtidoPor: "respondeai" },
+        { ftUsuario: "./assets/img/barked.svg", usrPost: "barked", conteudo: "./assets/img/dog.svg", imgCurtidoPor: "./assets/img/adorable_animals.svg", curtidoPor: "adorable_animals" },
+        { ftUsuario: "./assets/img/meowed.svg", usrPost: "meowed", conteudo: "./assets/img/gato-telefone.svg", imgCurtidoPor: "./assets/img/barked.svg", curtidoPor: "barked" }
+    ]
     return (
         <div className="posts">
-            <Post fotoUsuario="./assets/img/meowed.svg" usuarioPost="meowed" conteudo="./assets/img/gato-telefone.svg" imagemCurtidoPor="./assets/img/respondeai.svg" curtidoPor="respondeai" />
-            <Post fotoUsuario="./assets/img/barked.svg" usuarioPost="barked" conteudo="./assets/img/dog.svg" imagemCurtidoPor="./assets/img/adorable_animals.svg" curtidoPor="adorable_animals" />
-            <Post fotoUsuario="./assets/img/meowed.svg" usuarioPost="meowed" conteudo="./assets/img/gato-telefone.svg" imagemCurtidoPor="./assets/img/barked.svg" curtidoPor="barked" />
-
+            <ul>
+                {feed.map((f) => <Post ftUsuario={f.ftUsuario} usrPost={f.usrPost} conteudo={f.conteudo} imgCurtidoPor={f.imgCurtidoPor} curtidoPor={f.curtidoPor} />)}
+            </ul>
         </div>
     )
 }
